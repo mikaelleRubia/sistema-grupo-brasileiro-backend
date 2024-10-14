63ee3818aa5745f8676a9e99d3920f48df93df1b
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-10-14 15:41:31 -0300

    fix: cors config para sistema-grupo-brasileiro.netlify

--- src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/infra/security/SecurityConfig.java
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/infra/security/SecurityConfig.java
@@ -55,7 +55,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200", 
+            "https://sistema-grupo-brasileiro.netlify.app/"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); 
         configuration.setAllowCredentials(true); 
