adaa3fcc6f70e704ff6504804f68f38617c95783
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2024-11-11 23:16:41 -0300

    0.0.24

--- src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/infra/security/CorsConfig.java
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/infra/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.infra.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,7 +15,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                            "http://localhost:4200", 
+                            "https://sistema-gb-front.onrender.com/"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
