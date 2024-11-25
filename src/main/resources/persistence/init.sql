b9c6763ab283f9ac4db3b9537b16dd155eb3456b
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2024-11-25 17:34:56 -0300

    0.0.31

--- src/main/resources/persistence/init.sql
+++ src/main/resources/persistence/init.sql
@@ -2,7 +2,6 @@ DO
 $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'grupobrasileiro') THEN
-        PERFORM dblink_connect('dbname=postgres');
         EXECUTE 'CREATE DATABASE grupobrasileiro';
     END IF;
 END
